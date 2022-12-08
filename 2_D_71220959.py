47f4dbae377e77d803028200789ce6dba94f012f
agushendra77903 <117900786+agushendra77903@users.noreply.github.com>
2022-12-08 16:33:43 +0700

    Add files via upload

--- 2_D_71220959.py
+++ 2_D_71220959.py
@@ -3,12 +3,15 @@ print("pilihan model matematika")
 print("1.perkalian")
 print("2.pembagian")
 kata = int(input("masukkan model matematika yang di inginkan 1/2:"))
-numbers= int(input("menampilkan tabel matematika dari angka : "))
+if kata == 1:
+    numbers= int(input("menampilkan tabel matematika dari angka : "))
+    for i in range(1, 11):
+        print(numbers, 'x', i, '=', numbers*i)
 
-for i in range(1, 11):
-    print(numbers, 'x', i, '=', numbers*i)
-#-
-kata = int(input("masukkan model matematika yang di inginkan 1/2:"))
-numbers= int(input("menampilkan tabel matematika dari angka : "))
-for i in range (50, 65):
-    print(numbers,':', i, '=', i/numbers )
+elif kata == 2:
+    numbers= int(input("menampilkan tabel matematika dari angka : "))
+    for i in range (50, 66):
+        print(numbers,':', i, '=', i/numbers )
+
+else:
+    print("")
